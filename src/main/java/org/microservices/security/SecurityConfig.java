135dead541d9567916b1b51d01a65c1e6b57874f
Houssem Moussa <houssem.moussa@elyadata.com>
2023-05-03 17:10:54 +0200

    add security config

---
+++ src/main/java/org/microservices/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.microservices.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers( "/login/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.csrf().disable();
+    }
+}
